8bc7921f2e7b1643f2a33d7ca5fa1dbbaebda4bd
CC <cmail4public@gmail.com>
2021-08-03 11:20:26 +0800

    For msm8974, replacing LOCAL_COPY_HEADERS with LOCAL_EXPORT_C_INCLUDE_DIRS, for VNDK

--- msm8974/libqdutils/Android.mk
+++ msm8974/libqdutils/Android.mk
@@ -15,8 +15,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
-LOCAL_COPY_HEADERS              := qdMetaData.h
+LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
 LOCAL_MODULE_PATH               := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
